84c5bb73cc90344074417e1e1cf6f2bd9219911c
jsenger1 <jsenger1@css.edu>
2018-11-08 11:28:23 -0600

    Created a Asynch decorator in a new class with the ITradeStorage interface that updates the database done in a seperate task.

---
+++ CurrencyTrader.AdoNet/SynchTradeStorage.cs
@@ -0,0 +1,27 @@
+﻿using CurrencyTrader.AdoNet;
+using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple.AdoNet
+{
+    public class SynchTradeStorage : ITradeStorage
+    {
+        ITradeStorage synchTradeStroage;
+        private ILogger logger;
+
+        public SynchTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            synchTradeStroage = new AdoNetTradeStorage(logger);
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            Task.Run(() => synchTradeStroage.Persist(trades));
+        }
+    }
+}
