290488409e440ee3bf36ea51b520796a466d13d2
jsenger1 <jsenger1@css.edu>
2018-11-08 11:30:37 -0600

    changed to correct namespace

--- CurrencyTrader.AdoNet/SynchTradeStorage.cs
+++ CurrencyTrader.AdoNet/SynchTradeStorage.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SingleResponsibilityPrinciple.AdoNet
+namespace CurrencyTrader.AdoNet
 {
     public class SynchTradeStorage : ITradeStorage
     {
